f5bd452196e9787b4c569881681df7ff18496846
KrzysztofMarzeda <104913817+KrzysztofMarzeda@users.noreply.github.com>
2022-05-04 17:17:22 +0200

    Update game.php

--- game.php
+++ game.php
@@ -4,20 +4,20 @@
 
 <!DOCTYPE HTML>
 <html lang = "pl">
-<head>
-	<meta charset = "utf-8" />
-	<title>Osadnicy</title>
-</head>
-<body>
-	<?php
-		echo "<p>Witaj ".$_SESSION['user'].'! [<a href="logout.php">Wyloguj się</a>]</p>';
-		echo "<p><b>Drewno</b>: ".$_SESSION['drewno'];
-		echo " | <b>Kamień</b>: ".$_SESSION['kamien'];
-		echo " | <b>Zboże</b>: ".$_SESSION['zboze']."</p>";
-		echo "<p><b>E-mail</b>: ".$_SESSION['email'];
-		echo "<br /><b>Data wygaśnięcia premium</b>: ".$_SESSION['dnipremium']."</p>";
+	<head>
+		<meta charset = "utf-8" />
+		<title>Osadnicy</title>
+	</head>
+	<body>
+		<?php
+			echo "<p>Witaj ".$_SESSION['user'].'! [<a href="logout.php">Wyloguj się</a>]</p>';
+			echo "<p><b>Drewno</b>: ".$_SESSION['drewno'];
+			echo " | <b>Kamień</b>: ".$_SESSION['kamien'];
+			echo " | <b>Zboże</b>: ".$_SESSION['zboze']."</p>";
+			echo "<p><b>E-mail</b>: ".$_SESSION['email'];
+			echo "<br /><b>Data wygaśnięcia premium</b>: ".$_SESSION['dnipremium']."</p>";
 			
-	?>
+		?>
 
-</body>
+	</body>
 </html>
