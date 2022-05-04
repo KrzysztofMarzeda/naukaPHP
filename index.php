1ed24183b9b0dd9a05acfa506727b8fcb752cd28
KrzysztofMarzeda <104913817+KrzysztofMarzeda@users.noreply.github.com>
2022-05-04 17:18:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,27 +10,27 @@
 
 <!DOCTYPE HTML>
 <html lang = "pl">
-<head>
-	<meta charset = "utf-8" />
-	<title>Osadnicy</title>
-</head>
-<body>
-	Tylko martwi ujrzeli koniec wojny - Platon <br /><br />
-	
-	<a href = "registration.php">Rejestracja - załóż darmowe konto!</a> <br /><br />
+	<head>
+		<meta charset = "utf-8" />
+		<title>Osadnicy</title>
+	</head>
+	<body>
+		Tylko martwi ujrzeli koniec wojny - Platon <br /><br />
 
-	<form action = "logowanie.php" method = "post">
-		Login:<br /> <input type = "text" name = "login"><br />
-		Hasło:<br /> <input type = "password" name = "pwd"><br /><br />
-		<input type = "submit" value = "Zaloguj"><br />
-		<?php
-			if(isset($_SESSION['error']))
-			{
-				echo $_SESSION['error'];
-			}
-			
-		?>		
-	</form>
+		<a href = "registration.php">Rejestracja - załóż darmowe konto!</a> <br /><br />
 
-</body>
+		<form action = "logowanie.php" method = "post">
+			Login:<br /> <input type = "text" name = "login"><br />
+			Hasło:<br /> <input type = "password" name = "pwd"><br /><br />
+			<input type = "submit" value = "Zaloguj"><br />
+			<?php
+				if(isset($_SESSION['error']))
+				{
+					echo $_SESSION['error'];
+				}
+
+			?>		
+		</form>
+
+	</body>
 </html>
